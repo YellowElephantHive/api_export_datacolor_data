ae0667b6815cc27f9b342d502b91dfbe1cb156a2
laiyuchun <wayne910139@gmail.com>
2020-04-15 11:55:28 +0800

    firstly, Expect to add connection of sqlite3 in app.py. Secondly, modify the structure of config.cfg and add database name. Thirdly, continue test business logic

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import sys
 import flask
 from flask import Flask
 from flask_restful import Resource, Api
+import sqlite3
+
+cwd = os.getcwd()
+con = sqlite3.connect(database=)
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,6 +24,7 @@ api.add_resource(Working, '/')
 @api.route('/api/get/data_color')
 class DataColor(Resource):
     def get(self):
+        pass
         
 
 if __name__ == '__main__':
